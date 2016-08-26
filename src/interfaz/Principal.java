80bbbf5cc7da5c369d0e1139df73b934aa3bf14d
jeison <jeisongalindo14@gmail.com>
2016-08-26 11:16:29 -0500

    desabilitar boton mostrar al cancelar

--- src/interfaz/Principal.java
+++ src/interfaz/Principal.java
@@ -191,6 +191,7 @@ public class Principal extends javax.swing.JFrame {
     private void cmdLlenarManualActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdLlenarManualActionPerformed
         double n;
         int sw,res;
+        boolean aux =true;
         for (int i = 0; i < v.length; i++) {
             do{
                 sw=1;
@@ -207,6 +208,7 @@ public class Principal extends javax.swing.JFrame {
                 if(res == 0){
                     sw=1;
                     i = v.length;
+                    aux =false;
                 }else{
                     sw=0;
                 }
@@ -219,7 +221,7 @@ public class Principal extends javax.swing.JFrame {
             cmdCrear.setEnabled(false);
             cmdLlenarManual.setEnabled(false);
             cmdLlenarAutom.setEnabled(false);
-            cmdMostrar.setEnabled(true);
+            cmdMostrar.setEnabled(aux);
             cmdBorrar.setEnabled(true);
     }//GEN-LAST:event_cmdLlenarManualActionPerformed
 
